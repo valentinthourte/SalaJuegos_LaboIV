b068c29713d752492c2a8dd2a34d75ff2c7bf6f9
vthourte <valentinthourte@gmail.com>
2025-04-28 10:12:54 -0300

    Remuevo environment.ts para evitar credenciales en la nube

--- SalaJuegos_LaboIV/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://wiwtpafmxrfqcbitbclz.supabase.co',
-    publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpd3RwYWZteHJmcWNiaXRiY2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxNTUzODIsImV4cCI6MjA1OTczMTM4Mn0.cbxvJsfHp_Aiq5R9iRTXhCid7NZHRSjHaSkVRaAa_8A'
-  };
